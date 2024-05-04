9bc1d973e7cfa0eb44877ed3eaef0b5d4c49e701
Guy Luz <guyluz11@gmail.com>
2024-05-05 00:24:37 +0300

    Adding description to welcome page

--- lib/presentation/organisms/intro/welcome_organism.dart
+++ lib/presentation/organisms/intro/welcome_organism.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:infinite_horizons/presentation/atoms/atoms.dart';
 
 class WelcomeOrganism extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/cbj_happy.png');
+    return Column(
+      children: [
+        const TextAtom(
+          "Sometimes we study new material but can't remember as much as we would like to.\nSometimes we work but our focus isn't as strong as we know it could be.\nUsing this app your study and work session efficiency can increase dramatically by following methods that got approved by science.",
+        ),
+        SeparatorAtom(),
+        Image.asset('assets/cbj_happy.png'),
+      ],
+    );
   }
 }
