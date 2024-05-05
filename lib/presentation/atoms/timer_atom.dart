387dceab9a5b983f7f428dea09a819abe0ccf31b
Vijaykarthik T K <39937989+vijaykarthiktk@users.noreply.github.com>
2024-05-05 17:10:22 +0530

    Implemented pause/continue buttons for timer. (#17)

--- lib/presentation/atoms/timer_atom.dart
+++ lib/presentation/atoms/timer_atom.dart
@@ -1,10 +1,12 @@
-import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'dart:math';
+
+import 'package:custom_timer/custom_timer.dart';
 import 'package:flutter/material.dart';
 
 class TimerAtom extends StatelessWidget {
   const TimerAtom(this.controller, this.timer, this.callback);
 
-  final CountDownController controller;
+  final CustomTimerController controller;
   final Duration timer;
   final VoidCallback callback;
 
@@ -18,25 +20,60 @@ class TimerAtom extends StatelessWidget {
     final ThemeData themeData = Theme.of(context);
     final ColorScheme colorScheme = themeData.colorScheme;
 
-    return CircularCountDownTimer(
-      duration: timer.inSeconds,
-      controller: controller,
-      width: MediaQuery.of(context).size.width / 2,
-      height: MediaQuery.of(context).size.height / 2,
-      ringColor: Colors.grey[300]!,
-      fillColor: Colors.purpleAccent[100]!,
-      strokeWidth: 20.0,
-      strokeCap: StrokeCap.round,
-      textStyle: TextStyle(
-        fontSize: 33.0,
-        color: colorScheme.onBackground,
-        fontWeight: FontWeight.bold,
-      ),
-      textFormat: CountdownTextFormat.MM_SS,
-      isReverseAnimation: true,
-      isReverse: true,
-      onComplete: onComplete,
-      isControlButtonShown: true,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40.0),
+      child: CustomTimer(
+          controller: controller,
+          builder: (state, time) {
+            return SizedBox(
+              width: min(MediaQuery.of(context).size.width / 2, MediaQuery.of(context).size.height / 2),
+              child: AspectRatio(
+                aspectRatio: 1.0,
+                child: ValueListenableBuilder(
+                  valueListenable: controller.state,
+                  builder: (context, value, child) {
+                    return GestureDetector(
+                      onTap: () {
+                        if (value == CustomTimerState.counting) {
+                          controller.pause();
+                        } else {
+                          controller.start();
+                        }
+                      },
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: CircularProgressIndicator(
+                              backgroundColor: Colors.grey[300],
+                              color: value == CustomTimerState.counting
+                                  ? Colors.purpleAccent[100]!
+                                  : Colors.grey[500],
+                              value: time.duration.inMilliseconds /
+                                  timer.inMilliseconds,
+                              strokeWidth: 22,
+                              strokeCap: StrokeCap.round,
+                            ),
+                          ),
+                          Align(
+                            alignment: FractionalOffset.center,
+                            child: Text(
+                              "${time.minutes}:${time.seconds}",
+                              style: TextStyle(
+                                fontSize: 33.0,
+                                color: colorScheme.onBackground,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          }),
     );
   }
 }
