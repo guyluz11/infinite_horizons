5c7463dd393735c0d088ca2e70387d176394f110
Madhav Gupta <e22cseu0827@bennett.edu.in>
2024-05-15 17:14:29 +0530

    feat: Moved DND Buisness logic to infrastructure folder, added different behaviour for different platforms

--- lib/presentation/organisms/intro/tips_organism.dart
+++ lib/presentation/organisms/intro/tips_organism.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_horizons/domain/study_type_abstract.dart';
 import 'package:infinite_horizons/domain/tip.dart';
+import 'package:infinite_horizons/infrastructure/core/dnd.dart';
 import 'package:infinite_horizons/presentation/atoms/atoms.dart';
 import 'package:infinite_horizons/presentation/molecules/molecules.dart';
 import 'package:infinite_horizons/presentation/pages/all_tips_page.dart';
@@ -39,23 +42,14 @@ class TipsOrganism extends StatelessWidget {
           ),
           const SeparatorAtom(variant: SeparatorVariant.farApart),
           Center(
-              child: ElevatedButton(
-                  onPressed: () async {
-                    bool filter =
-                        await FlutterDnd.getCurrentInterruptionFilter() ==
-                            FlutterDnd.INTERRUPTION_FILTER_PRIORITY;
-                    if (await FlutterDnd.isNotificationPolicyAccessGranted ??
-                        false) {
-                      await FlutterDnd.setInterruptionFilter(
-                        filter
-                            ? FlutterDnd.INTERRUPTION_FILTER_ALL
-                            : FlutterDnd.INTERRUPTION_FILTER_PRIORITY,
-                      ); // Turn on DND - All notifications are suppressed.
-                    } else {
-                      FlutterDnd.gotoPolicySettings();
-                    }
-                  },
-                  child: Text("Toggle DND"))),
+            child: ButtonAtom(
+              variant: ButtonVariant.primary,
+              onPressed: () {
+                toggleDND(context);
+              },
+              text: Platform.isAndroid ? 'dnd' : 'dnd_ios',
+            ),
+          ),
           ListView.builder(
             shrinkWrap: true,
             itemBuilder: (BuildContext context, int index) {
