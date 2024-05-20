7d125da632f21049418ba0002227d37fc734d50b
Madhav Gupta <e22cseu0827@bennett.edu.in>
2024-05-20 16:15:28 +0530

    fix: Remove unused import

--- lib/presentation/pages/tip_information_page.dart
+++ lib/presentation/pages/tip_information_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:infinite_horizons/domain/tip.dart';
 import 'package:infinite_horizons/presentation/atoms/atoms.dart';
 import 'package:infinite_horizons/presentation/molecules/molecules.dart';
