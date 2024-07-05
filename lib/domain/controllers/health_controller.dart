61bcebcac4869e6ffb470a6df2fc7c629e4a4b9e
Guy Luz <guyluz11@gmail.com>
2024-07-05 15:28:50 +0300

    Android minSdkVersion 26 for health package

--- lib/domain/controllers/health_controller.dart
+++ lib/domain/controllers/health_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:health/health.dart';
+
 import 'package:infinite_horizons/domain/controllers/permissions_controller.dart';
 
 part 'package:infinite_horizons/infrastructure/health_repository.dart';
