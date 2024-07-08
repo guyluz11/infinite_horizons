312c84bf13d2ecd4e81e87338a98de03b795e5f4
Guy Luz <guyluz11@gmail.com>
2024-07-08 11:19:59 +0300

    Adding permissions options to settings page

--- lib/presentation/pages/settings_page.dart
+++ lib/presentation/pages/settings_page.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:infinite_horizons/domain/controllers/controllers.dart';
 import 'package:infinite_horizons/presentation/atoms/atoms.dart';
 import 'package:infinite_horizons/presentation/molecules/molecules.dart';
+import 'package:infinite_horizons/presentation/organisms/organisms.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -23,55 +24,61 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return PageEnclosureMolecule(
       title: 'settings',
-      child: Column(
-        children: [
-          CardAtom(
-            child: ToggleSwitchMolecule(
-              text: 'sound',
-              offIcon: Icons.music_off_rounded,
-              onIcon: Icons.music_note_rounded,
-              onChange: (bool value) {
-                PlayerController.instance.setIsSound(value);
-                _prefs.setBool(PreferenceKeys.isSound.name, value);
-              },
-              initialValue: PlayerController.instance.isSound(),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CardAtom(
+              child: ToggleSwitchMolecule(
+                text: 'sound',
+                description: 'Allow app to play sound effects',
+                offIcon: Icons.music_off_rounded,
+                onIcon: Icons.music_note_rounded,
+                onChange: (bool value) {
+                  PlayerController.instance.setIsSound(value);
+                  _prefs.setBool(PreferenceKeys.isSound.name, value);
+                },
+                initialValue: PlayerController.instance.isSound(),
+              ),
             ),
-          ),
-          const SeparatorAtom(),
-          CardAtom(
-            child: ToggleSwitchMolecule(
-              text: 'screen_lock',
-              offIcon: Icons.lock_clock,
-              onIcon: Icons.lock_open,
-              onChange: (bool value) {
-                lockScreen = value;
-                _prefs.setBool(PreferenceKeys.isLockScreen.name, lockScreen);
-                WakeLockController.instance.setWakeLock(lockScreen);
-              },
-              initialValue: lockScreen,
+            const SeparatorAtom(),
+            CardAtom(
+              child: ToggleSwitchMolecule(
+                text: 'screen_lock',
+                description: 'When on screen will not get locked on timer page',
+                offIcon: Icons.lock_clock,
+                onIcon: Icons.lock_open,
+                onChange: (bool value) {
+                  lockScreen = value;
+                  _prefs.setBool(PreferenceKeys.isLockScreen.name, lockScreen);
+                  WakeLockController.instance.setWakeLock(lockScreen);
+                },
+                initialValue: lockScreen,
+              ),
             ),
-          ),
-          const SeparatorAtom(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              ButtonAtom(
-                variant: ButtonVariant.lowEmphasisIcon,
-                onPressed: () => WebBrowserController.instance.lunchLink(
-                  'https://github.com/guyluz11/infinite_horizons/issues',
+            const SeparatorAtom(),
+            const PermissionsOrganism(isPageEnclosure: false),
+            const SeparatorAtom(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ButtonAtom(
+                  variant: ButtonVariant.lowEmphasisIcon,
+                  onPressed: () => WebBrowserController.instance.lunchLink(
+                    'https://github.com/guyluz11/infinite_horizons/issues',
+                  ),
+                  icon: FontAwesomeIcons.circleDot,
                 ),
-                icon: FontAwesomeIcons.circleDot,
-              ),
-              ButtonAtom(
-                variant: ButtonVariant.lowEmphasisIcon,
-                onPressed: () => WebBrowserController.instance
-                    .lunchLink('https://github.com/guyluz11/infinite_horizons'),
-                icon: FontAwesomeIcons.github,
-              ),
-            ],
-          ),
-          const SeparatorAtom(variant: SeparatorVariant.farApart),
-        ],
+                ButtonAtom(
+                  variant: ButtonVariant.lowEmphasisIcon,
+                  onPressed: () => WebBrowserController.instance.lunchLink(
+                      'https://github.com/guyluz11/infinite_horizons'),
+                  icon: FontAwesomeIcons.github,
+                ),
+              ],
+            ),
+            const SeparatorAtom(variant: SeparatorVariant.farApart),
+          ],
+        ),
       ),
     );
   }
